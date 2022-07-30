7a227a2f96ce13110bff503dfa9ccfb9df636537
Igor <igor,zhuiko@mail.ru>
2022-07-30 17:51:27 +0300

    added modal window

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
+import { CreateProduct } from "./components/CreateProduct";
 import { ErrorMesage } from "./components/ErrorMessage";
 import { Loader } from "./components/Loader";
+import { Modal } from "./components/Modal";
 import { Product } from "./components/Product";
 import { useProducts } from "./hooks/products";
 
 function App() {
   const {loading, error, products} = useProducts()
+  const [modal, setModal] = useState(true)
   
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      {loading && <Loader />}
-      {error && <ErrorMesage error={error}/>}
-      {products.map(product => <Product product={product} key={product.id}/>)}
+      { loading && <Loader /> }
+      { error && <ErrorMesage error={error}/> }
+      { products.map(product => <Product product={product} key={product.id}/>) }
+
+      {modal && <Modal title="Create new product">
+        <CreateProduct />
+      </Modal>}
     </div>
+    
   );
 }
 
