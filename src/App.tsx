5a3bef6212ce13057f612892c1637cf295398d32
Igor <igor,zhuiko@mail.ru>
2022-07-30 15:44:19 +0300

    state products and response server

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product';
-import { products } from './data/products';
+// import { products } from './data/products';
+import { IProduct } from './models';
 
 
 function App() {
+
+  const [products, setProducts] = useState<IProduct[]>([])
+
+  async function fetchProducts() {
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=7')
+    setProducts(response.data)
+    
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  },[])
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
       {products.map(product => <Product product={product} key={product.id}/>)}
