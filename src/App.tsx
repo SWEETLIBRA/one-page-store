0ad9aad074788a3c6ba6d50d2ca9704f2874997c
Igor <igor,zhuiko@mail.ru>
2022-07-30 16:22:39 +0300

    improved the structure of the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,15 @@
-import axios, { AxiosError } from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Product } from './components/Product';
-// import { products } from './data/products';
-import { IProduct } from './models';
-
+import { ErrorMesage } from "./components/ErrorMessage";
+import { Loader } from "./components/Loader";
+import { Product } from "./components/Product";
+import { useProducts } from "./hooks/products";
 
 function App() {
-
-  const [products, setProducts] = useState<IProduct[]>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-
-  async function fetchProducts() {
-    try {
-      setError('')
-      setLoading(true)
-      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=7')
-      setProducts(response.data)
-      setLoading(false)
-    } catch (e: unknown) {
-      const error = e as AxiosError
-      setLoading(false)
-      setError(error.message)
-    }
-  }
-
-  useEffect(() => {
-    fetchProducts()
-  },[])
+  const {loading, error, products} = useProducts()
+  
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      {loading && <p className='text-center'>Loading...</p>}
-      {error && <p className='text-center text-red-600'>{ error }</p>}
+      {loading && <Loader />}
+      {error && <ErrorMesage error={error}/>}
       {products.map(product => <Product product={product} key={product.id}/>)}
     </div>
   );
