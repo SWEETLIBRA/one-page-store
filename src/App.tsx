8e5766d15af247a5b8bcc2dc4903bec88002f3c6
Igor <igor,zhuiko@mail.ru>
2022-08-01 18:40:48 +0300

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,19 @@
-import { useContext } from "react";
-import { CreateProduct } from "./components/CreateProduct";
-import { ErrorMesage } from "./components/ErrorMessage";
-import { Loader } from "./components/Loader";
-import { Modal } from "./components/Modal";
-import { Product } from "./components/Product";
-import { ModalContext } from "./context/ModalContext";
-import { useProducts } from "./hooks/products";
-import { IProduct } from "./models";
+import { Route, Routes } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
+import { AboutPage } from "./pages/AboutPage";
+import { ProductPage } from "./pages/ProductPage";
+
 
 function App() {
-  const {loading, error, products, addProducts} = useProducts()
-  const {modal, open, close} = useContext(ModalContext)
-
-  const createHandler = (product: IProduct) => {
-    close()
-    addProducts(product)
-  }
-  
   return (
-    <div className='container bg-lime-50 mx-auto max-w-2xl pt-5'>
-      { loading && <Loader /> }
-      { error && <ErrorMesage error={error}/> }
-      { products.map(product => <Product product={product} key={product.id}/>) }
-
-      {modal && <Modal title="Create new product" onClose={close}>
-        <CreateProduct onCreate={createHandler}/>
-      </Modal>}
-
-      <button 
-        className="fixed bottom-5 right-5 rounded-full bg-cyan-500 shadow-lg shadow-cyan-500/50 text-white text-2xl px-4 py-2"
-        onClick={open}
-      > +
-      </button>
-    </div>
-    
-  );
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<ProductPage /> } />
+        <Route path="/about" element={<AboutPage /> } />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function AboutPage() {
+    return (
+        <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, optio amet tempore quod qui mollitia corporis est minima maxime error atque, libero magni veniam laborum eos, adipisci sapiente facilis! Quibusdam, ipsa dolor quia rem perferendis delectus, at culpa laboriosam corporis sint autem animi, voluptatem aut maiores fugiat obcaecati! Ullam, harum?
+        </div>
+    )
+}
